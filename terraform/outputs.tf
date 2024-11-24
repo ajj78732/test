f955ed1054b44eb82e6af147605757a35d852891
ajj78732 <ajj78732@gmail.com>
2024-11-25 04:03:49 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,35 +1,15 @@
-# outputs.tf
 output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
 output "subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
 
 output "security_group_id" {
-  description = "ID of the security group"
-  value       = aws_security_group.main.id
+  value = aws_security_group.main.id
 }
 
 output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.main.dns_name
-}
-
-output "target_group_arn" {
-  description = "ARN of the Target Group"
-  value       = aws_lb_target_group.main.arn
-}
-
-output "autoscaling_group_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.main.name
-}
-
-output "key_name" {
-  description = "Name of the created key pair"
-  value       = aws_key_pair.deployer.key_name
+  value = aws_lb.main.dns_name
 }
