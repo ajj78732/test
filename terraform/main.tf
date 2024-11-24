1a9ec7d1c28114639568f39b27eb0c1db472fcdf
ajj78732 <ajj78732@gmail.com>
2024-11-25 03:52:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,7 +132,7 @@ resource "aws_launch_template" "main" {
   image_id      = var.ami_id
   instance_type = var.instance_type
 
-  network_interface {
+  network_interfaces {
     associate_public_ip_address = true
     security_groups            = [aws_security_group.main.id]
   }
