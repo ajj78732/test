dffe76fb09c7be58ee3a25c90a544cf8d6560aa0
ajj78732 <ajj78732@gmail.com>
2024-11-25 02:30:52 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,37 +1,64 @@
+# variables.tf
 variable "aws_region" {
   description = "AWS region"
+  type        = string
   default     = "us-east-1"
 }
 
+variable "project_name" {
+  description = "Project name to be used for tagging"
+  type        = string
+  default     = "flask-app"
+}
+
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "CIDR block for VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
-  description = "CIDR block for the subnet"
+  description = "CIDR block for subnet"
+  type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "key_name" {
-  description = "SSH key pair name"
-  default     = "jenkins-ec2-key"
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI in us-east-1
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "docker_image" {
   description = "Docker image to deploy"
-}
-variable "min_size" {
-  default = 2
-  description = "Minimum number of instances in the auto-scaling group"
+  type        = string
 }
 
-variable "max_size" {
-  default = 2
-  description = "Maximum number of instances in the auto-scaling group"
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
 }
 
-variable "desired_capacity" {
-  default = 1
-  description = "Desired number of instances in the auto-scaling group"
+variable "asg_desired_capacity" {
+  description = "Desired number of instances in ASG"
+  type        = number
+  default     = 2
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of instances in ASG"
+  type        = number
+  default     = 4
+}
+
+variable "asg_min_size" {
+  description = "Minimum number of instances in ASG"
+  type        = number
+  default     = 1
 }
