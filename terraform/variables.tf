d4a986f7dab7308cabb57a0fbe282633663a5fa7
ajj78732 <ajj78732@gmail.com>
2024-11-25 02:49:56 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,20 +12,28 @@ variable "vpc_cidr" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
+  default     = "flask-app"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instances"
   type        = string
+  default     = "ami-05c13eab67c5d8861"  # Amazon Linux 2023 AMI in us-east-1
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
+  default     = "t2.micro"
 }
 
 variable "docker_image" {
   description = "Docker image to run on the instances"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
   type        = string
 }
 
