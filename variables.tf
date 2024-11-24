6142176bfd7c7da7e16300f26424072d9704a845
ajj78732 <ajj78732@gmail.com>
2024-11-25 01:05:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,22 @@
 variable "aws_region" {
-  description = "AWS region for resources"
+  description = "AWS region"
   default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
 }
 
 variable "docker_image" {
   description = "Docker image to deploy"
-}
-
-variable "key_name" {
-  description = "Key name for EC2 instance SSH access"
 }
