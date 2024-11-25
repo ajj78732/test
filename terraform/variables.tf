72d3b2668ccd3846da7418549cb155b55a247f69
ajj78732 <ajj78732@gmail.com>
2024-11-25 18:44:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,12 @@ variable "docker_image" {}
 variable "asg_desired_capacity" { default = 1 }
 variable "asg_max_size" { default = 2 }
 variable "asg_min_size" { default = 1 }
+variable "deployment_mode" {
+  description = "Deployment mode (apply or destroy)"
+  type        = string
+  default     = "apply"
+  validation {
+    condition     = contains(["apply", "destroy"], var.deployment_mode)
+    error_message = "Deployment mode must be either 'apply' or 'destroy'."
+  }
+}
